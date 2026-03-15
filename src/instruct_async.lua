a73481810600c32ed04e025dcf5bb868bbdac0f9
wangmuy <wangmuy@gmail.com>
2026-03-15 14:43:03 +0800

    Implement TalkAsync and more instruct_async functions
    - Create talk_async.lua with TalkExCoroutine
    - Implement instruct_27 (animation), instruct_30 (walk)
    - Implement instruct_58 (tournament), instruct_64 (shop)
    - Add state history to StateMachine
    - Progress: 40/93 tasks complete

--- src/instruct_async.lua
+++ src/instruct_async.lua
@@ -8,6 +8,7 @@ local CoroutineScheduler = require("coroutine_scheduler")
 local AsyncMessageBox = require("async_message_box")
 local InputAsync = require("input_async")
 local MenuAsync = require("menu_async")
+local TalkAsync = require("talk_async")
 
 -- 获取调度器实例
 local function getScheduler()
@@ -84,66 +85,224 @@ function InstructAsync.instruct_15()
     love.event.quit()
 end
 
+-- instruct_27: 显示动画（协程版本）
+function InstructAsync.instruct_27(id, startpic, endpic)
+    local scheduler = getScheduler()
+    local old1, old2, old3
+    
+    if id ~= -1 then
+        old1 = GetD(JY.SubScene, id, 5)
+        old2 = GetD(JY.SubScene, id, 6)
+        old3 = GetD(JY.SubScene, id, 7)
+    end
+    
+    for i = startpic, endpic, 2 do
+        local t1 = lib.GetTime()
+        
+        if id == -1 then
+            JY.MyPic = i / 2
+        else
+            SetD(JY.SubScene, id, 5, i)
+            SetD(JY.SubScene, id, 6, i)
+            SetD(JY.SubScene, id, 7, i)
+        end
+        
+        DtoSMap()
+        DrawSMap()
+        ShowScreen()
+        
+        local t2 = lib.GetTime()
+        if t2 - t1 < CC.AnimationFrame then
+            scheduler:waitForTime((CC.AnimationFrame - (t2 - t1)) / 1000)
+        end
+    end
+    
+    if id ~= -1 then
+        SetD(JY.SubScene, id, 5, old1)
+        SetD(JY.SubScene, id, 6, old2)
+        SetD(JY.SubScene, id, 7, old3)
+    end
+end
+
+-- instruct_30: 主角走动（协程版本）
+function InstructAsync.instruct_30(x1, y1, x2, y2)
+    local scheduler = getScheduler()
+    
+    if x1 < x2 then
+        for i = x1 + 1, x2 do
+            local t1 = lib.GetTime()
+            instruct_30_sub(1)
+            local t2 = lib.GetTime()
+            if (t2 - t1) < CC.PersonMoveFrame then
+                scheduler:waitForTime((CC.PersonMoveFrame - (t2 - t1)) / 1000)
+            end
+        end
+    elseif x1 > x2 then
+        for i = x2 + 1, x1 do
+            local t1 = lib.GetTime()
+            instruct_30_sub(2)
+            local t2 = lib.GetTime()
+            if (t2 - t1) < CC.PersonMoveFrame then
+                scheduler:waitForTime((CC.PersonMoveFrame - (t2 - t1)) / 1000)
+            end
+        end
+    end
+    
+    if y1 < y2 then
+        for i = y1 + 1, y2 do
+            local t1 = lib.GetTime()
+            instruct_30_sub(3)
+            local t2 = lib.GetTime()
+            if (t2 - t1) < CC.PersonMoveFrame then
+                scheduler:waitForTime((CC.PersonMoveFrame - (t2 - t1)) / 1000)
+            end
+        end
+    elseif y1 > y2 then
+        for i = y2 + 1, y1 do
+            local t1 = lib.GetTime()
+            instruct_30_sub(0)
+            local t2 = lib.GetTime()
+            if (t2 - t1) < CC.PersonMoveFrame then
+                scheduler:waitForTime((CC.PersonMoveFrame - (t2 - t1)) / 1000)
+            end
+        end
+    end
+end
+
+-- instruct_58: 武道大会比武（协程版本）
+function InstructAsync.instruct_58()
+    local group = 5
+    local num1 = 6
+    local num2 = 3
+    local startwar = 102
+    local flag = {}
+    
+    for i = 0, group - 1 do
+        for j = 0, num1 - 1 do
+            flag[j] = 0
+        end
+        
+        for j = 1, num2 do
+            local r
+            while true do
+                r = Rnd(num1)
+                if flag[r] == 0 then
+                    flag[r] = 1
+                    break
+                end
+            end
+            
+            local warnum = r + i * num1
+            WarLoad(warnum + startwar)
+            
+            InstructAsync.instruct_1(2854 + warnum, JY.Person[WAR.Data["敌人1"]]["头像代号"], 0)
+            instruct_0()
+            
+            if WarMainCoroutine(warnum + startwar, 0) == true then
+                instruct_0()
+                instruct_13()
+                TalkAsync.TalkExCoroutine("还有那位前辈肯赐教？", 0, 1)
+                instruct_0()
+            else
+                InstructAsync.instruct_15()
+                return
+            end
+        end
+        
+        if i < group - 1 then
+            TalkAsync.TalkExCoroutine("少侠已连战三场，*可先休息再战．", 70, 0)
+            instruct_0()
+            instruct_14()
+            getScheduler():waitForTime(0.3)
+            
+            if JY.Person[0]["受伤程度"] < 50 and JY.Person[0]["中毒程度"] <= 0 then
+                JY.Person[0]["受伤程度"] = 0
+                AddPersonAttrib(0, "体力", math.huge)
+                AddPersonAttrib(0, "内力", math.huge)
+                AddPersonAttrib(0, "生命", math.huge)
+            end
+            
+            instruct_13()
+            TalkAsync.TalkExCoroutine("我已经休息够了，*有谁要再上？", 0, 1)
+            instruct_0()
+        end
+    end
+    
+    TalkAsync.TalkExCoroutine("接下来换谁？**．．．．*．．．．***没有人了吗？", 0, 1)
+    instruct_0()
+    TalkAsync.TalkExCoroutine("如果还没有人要出来向这位*少侠挑战，那麽这武功天下*第一之名，武林盟主之位，*就由这位少侠夺得．***．．．．．．*．．．．．．*．．．．．．*好，恭喜少侠，这武林盟主*之位就由少侠获得，而这把*"武林神杖"也由你保管．", 70, 0)
+    instruct_0()
+    TalkAsync.TalkExCoroutine("恭喜少侠！", 12, 0)
+    instruct_0()
+    TalkAsync.TalkExCoroutine("小兄弟，恭喜你！", 64, 4)
+    instruct_0()
+    TalkAsync.TalkExCoroutine("好，今年的武林大会到此已*圆满结束，希望明年各位武*林同道能再到我华山一游．", 19, 0)
+    instruct_0()
+    instruct_14()
+    
+    for i = 24, 72 do
+        instruct_3(-2, i, 0, 0, -1, -1, -1, -1, -1, -1, -2, -2, -2)
+    end
+end
+
+-- instruct_64: 小宝卖东西（协程版本）
+function InstructAsync.instruct_64()
+    local headid = 111
+    
+    local id = -1
+    for i = 0, JY.ShopNum - 1 do
+        if CC.ShopScene[i].sceneid == JY.SubScene then
+            id = i
+            break
+        end
+    end
+    if id < 0 then
+        id = 0
+    end
+    
+    TalkAsync.TalkExCoroutine("这位小哥，看看有什麽需要*的，小宝我卖的东西价钱绝*对公道．", headid, 0)
+    
+    local menu = {}
+    for i = 1, 5 do
+        menu[i] = {}
+        local thingid = JY.Shop[id]["物品" .. i]
+        menu[i][1] = string.format("%-12s %5d", JY.Thing[thingid]["名称"], JY.Shop[id]["物品价格" .. i])
+        menu[i][2] = nil
+        if JY.Shop[id]["物品数量" .. i] > 0 then
+            menu[i][3] = 1
+        else
+            menu[i][3] = 0
+        end
+    end
+    
+    local x1 = (CC.ScreenW - 9 * CC.DefaultFont - 2 * CC.MenuBorderPixel) / 2
+    local y1 = (CC.ScreenH - 5 * CC.DefaultFont - 4 * CC.RowPixel - 2 * CC.MenuBorderPixel) / 2
+    
+    local r = MenuAsync.ShowMenuCoroutine(menu, 5, 0, x1, y1, 0, 0, 1, 1, CC.DefaultFont, C_ORANGE, C_WHITE)
+    
+    if r > 0 then
+        if instruct_31(JY.Shop[id]["物品价格" .. r]) == false then
+            TalkAsync.TalkExCoroutine("非常抱歉，*你身上的钱似乎不够．", headid, 0)
+        else
+            JY.Shop[id]["物品数量" .. r] = JY.Shop[id]["物品数量" .. r] - 1
+            instruct_32(CC.MoneyID, -JY.Shop[id]["物品价格" .. r])
+            instruct_32(JY.Shop[id]["物品" .. r], 1)
+            TalkAsync.TalkExCoroutine("大爷买了我小宝的东西，*保证绝不後悔．", headid, 0)
+        end
+    end
+    
+    for i, v in ipairs(CC.ShopScene[id].d_leave) do
+        instruct_3(-2, v, 0, -2, -1, -1, 939, -1, -1, -1, -2, -2, -2)
+    end
+end
+
 -- ============================================
 -- 协程版本的辅助函数
 -- ============================================
 
 -- TalkEx 协程版本
 function TalkExCoroutine(s, headid, flag)
-    if s == nil then
-        return
-    end
-    
-    local function processTalkString(str)
-        local pos = 1
-        local len = string.len(str)
-        
-        while pos <= len do
-            local nextPage = string.find(str, "@@@", pos)
-            if nextPage then
-                local page = string.sub(str, pos, nextPage - 1)
-                TalkPageCoroutine(page, headid, flag)
-                pos = nextPage + 3
-            else
-                local page = string.sub(str, pos)
-                TalkPageCoroutine(page, headid, flag)
-                break
-            end
-        end
-    end
-    
-    processTalkString(s)
-    Cls()
-    ShowScreen()
-end
-
--- TalkPage 协程版本（单页对话）
-function TalkPageCoroutine(text, headid, flag)
-    local x, y
-    local fontsize = CC.DefaultFont
-    
-    if flag == 5 then
-        -- 屏幕下方对话框
-        x = 0
-        y = CC.ScreenH - fontsize - CC.RowPixel * 3
-    else
-        x = -1
-        y = -1
-    end
-    
-    Cls()
-    
-    -- 显示头像
-    if headid >= 0 then
-        lib.DrawHead(headid, x, y)
-    end
-    
-    -- 显示对话文本
-    DrawStrBox(x, y, text, C_WHITE, fontsize)
-    ShowScreen()
-    
-    -- 等待按键
-    InputAsync.WaitKeyCoroutine()
+    TalkAsync.TalkExCoroutine(s, headid, flag)
 end
 
 -- WarMain 协程版本（战斗主函数）
---
+++ src/talk_async.lua
@@ -0,0 +1,181 @@
+-- talk_async.lua
+-- 对话系统异步模块
+-- 提供对话显示的协程版本函数
+
+local TalkAsync = {}
+
+local CoroutineScheduler = require("coroutine_scheduler")
+local InputAsync = require("input_async")
+
+-- 对话框配置
+local talkConfig = {
+    picw = 100,       -- 最大头像图片宽
+    pich = 100,       -- 最大头像图片高
+    talkxnum = 12,    -- 对话一行字数
+    talkynum = 3,     -- 对话行数
+    dx = 2,
+    dy = 2,
+}
+
+-- 获取调度器实例
+local function getScheduler()
+    return CoroutineScheduler.getInstance()
+end
+
+-- 计算对话框位置配置
+local function getTalkPosition(flag)
+    local picw = talkConfig.picw
+    local pich = talkConfig.pich
+    local dx = talkConfig.dx
+    local dy = talkConfig.dy
+    local boxpicw = picw + 10
+    local boxpich = pich + 10
+    local boxtalkw = 12 * CC.DefaultFont + 10
+    local boxtalkh = boxpich
+    
+    local xy = {
+        [0] = {
+            headx = dx, heady = dy,
+            talkx = dx + boxpicw + 2, talky = dy,
+            showhead = 1
+        },
+        {
+            headx = CC.ScreenW - 1 - dx - boxpicw, heady = CC.ScreenH - dy - boxpich,
+            talkx = CC.ScreenW - 1 - dx - boxpicw - boxtalkw - 2, talky = CC.ScreenH - dy - boxpich,
+            showhead = 1
+        },
+        {
+            headx = dx, heady = dy,
+            talkx = dx + boxpicw + 2, talky = dy,
+            showhead = 0
+        },
+        {
+            headx = CC.ScreenW - 1 - dx - boxpicw, heady = CC.ScreenH - dy - boxpich,
+            talkx = CC.ScreenW - 1 - dx - boxpicw - boxtalkw - 2, talky = CC.ScreenH - dy - boxpich,
+            showhead = 1
+        },
+        {
+            headx = CC.ScreenW - 1 - dx - boxpicw, heady = dy,
+            talkx = CC.ScreenW - 1 - dx - boxpicw - boxtalkw - 2, talky = dy,
+            showhead = 1
+        },
+        {
+            headx = dx, heady = CC.ScreenH - dy - boxpich,
+            talkx = dx + boxpicw + 2, talky = CC.ScreenH - dy - boxpich,
+            showhead = 1
+        },
+    }
+    
+    if flag < 0 or flag > 5 then
+        flag = 0
+    end
+    
+    return xy[flag], boxtalkw, boxtalkh
+end
+
+-- 生成分行字符串
+local function GenTalkString(s, linelen)
+    local newstr = ""
+    local len = string.len(s)
+    local start = 1
+    
+    while start <= len do
+        local endp = start + linelen - 1
+        if endp > len then
+            endp = len
+        end
+        newstr = newstr .. string.sub(s, start, endp) .. "*"
+        start = endp + 1
+    end
+    
+    return newstr
+end
+
+-- 对话显示（协程版本）
+-- @param s: 对话字符串，用*分隔行
+-- @param headid: 头像ID，-1表示不显示
+-- @param flag: 对话框位置
+function TalkAsync.TalkExCoroutine(s, headid, flag)
+    lib.Debug(string.format("TalkExCoroutine: headid=%d, flag=%d", headid, flag))
+    
+    local picw = talkConfig.picw
+    local pich = talkConfig.pich
+    local talkynum = talkConfig.talkynum
+    local talkBorder = (pich - talkynum * CC.DefaultFont) / (talkynum + 1)
+    
+    local xy, boxtalkw, boxtalkh = getTalkPosition(flag)
+    
+    if xy.showhead == 0 then
+        headid = -1
+    end
+    
+    -- 自动分行
+    if string.find(s, "*") == nil then
+        s = GenTalkString(s, 12)
+    end
+    
+    lib.GetKey()
+    
+    local startp = 1
+    local endp
+    local dy = 0
+    
+    while true do
+        if dy == 0 then
+            Cls()
+            
+            -- 显示头像
+            if headid >= 0 then
+                local boxpicw = picw + 10
+                local boxpich = pich + 10
+                DrawBox(xy.headx, xy.heady, xy.headx + boxpicw, xy.heady + boxpich, C_WHITE)
+                
+                local w, h = lib.PicGetXY(1, headid * 2)
+                local x = (picw - w) / 2
+                local y = (pich - h) / 2
+                lib.PicLoadCache(1, headid * 2, xy.headx + 5 + x, xy.heady + 5 + y, 1)
+            end
+            
+            -- 绘制对话框
+            DrawBox(xy.talkx, xy.talky, xy.talkx + boxtalkw, xy.talky + boxtalkh, C_WHITE)
+        end
+        
+        endp = string.find(s, "*", startp)
+        
+        if endp == nil then
+            -- 最后一行
+            DrawString(xy.talkx + 5, xy.talky + 5 + talkBorder + dy * (CC.DefaultFont + talkBorder),
+                       string.sub(s, startp), C_WHITE, CC.DefaultFont)
+            ShowScreen()
+            InputAsync.WaitKeyCoroutine()
+            break
+        else
+            DrawString(xy.talkx + 5, xy.talky + 5 + talkBorder + dy * (CC.DefaultFont + talkBorder),
+                       string.sub(s, startp, endp - 1), C_WHITE, CC.DefaultFont)
+        end
+        
+        dy = dy + 1
+        startp = endp + 1
+        
+        if dy >= talkynum then
+            ShowScreen()
+            InputAsync.WaitKeyCoroutine()
+            dy = 0
+        end
+    end
+    
+    Cls()
+end
+
+-- 简单版本对话（协程版本）
+function TalkAsync.TalkCoroutine(s, personid)
+    local flag
+    if personid == 0 then
+        flag = 1
+    else
+        flag = 0
+    end
+    TalkAsync.TalkExCoroutine(s, JY.Person[personid]["头像代号"], flag)
+end
+
+return TalkAsync
