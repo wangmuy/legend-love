42c258e7fac38bc57100d812fd68559c526995ac
wangmuy <wangmuy@gmail.com>
2026-03-15 17:09:57 +0800

    Fix menu callback: don't use menu item callbacks
    Menu item callbacks are called outside coroutine context, causing yield errors.
    
    Solution:
    - MMenuCoroutine uses return value to decide actions
    - All menu functions are called directly in the main menu coroutine
    - No more callback functions in menu items

--- src/jymain_async.lua
+++ src/jymain_async.lua
@@ -16,29 +16,50 @@ local TalkAsync = require("talk_async")
 
 -- 主菜单（协程版本）
 function JyMainAsync.MMenuCoroutine()
-    local menu = {
-        {"医疗", JyMainAsync.Menu_DoctorCoroutine, 1},
-        {"解毒", JyMainAsync.Menu_DecPoisonCoroutine, 1},
-        {"物品", JyMainAsync.Menu_ThingCoroutine, 1},
-        {"状态", JyMainAsync.Menu_StatusCoroutine, 1},
-        {"离队", JyMainAsync.Menu_PersonExitCoroutine, 1},
-        {"系统", JyMainAsync.Menu_SystemCoroutine, 1},
-    }
-    
-    if JY.Status == GAME_SMAP then
-        menu[5][3] = 0
-        menu[6][3] = 0
+    while true do
+        local menu = {
+            {"医疗", nil, 1},
+            {"解毒", nil, 1},
+            {"物品", nil, 1},
+            {"状态", nil, 1},
+            {"离队", nil, 1},
+            {"系统", nil, 1},
+        }
+        
+        if JY.Status == GAME_SMAP then
+            menu[5][3] = 0
+            menu[6][3] = 0
+        end
+        
+        local r = MenuAsync.ShowMenuCoroutine(menu, 6, 0, CC.MainMenuX, CC.MainMenuY, 0, 0, 1, 1, CC.DefaultFont, C_ORANGE, C_WHITE)
+        
+        if r == 0 then
+            return  -- ESC退出菜单
+        elseif r == 1 then
+            JyMainAsync.Menu_Doctor()
+        elseif r == 2 then
+            JyMainAsync.Menu_DecPoison()
+        elseif r == 3 then
+            JyMainAsync.Menu_Thing()
+        elseif r == 4 then
+            JyMainAsync.Menu_Status()
+        elseif r == 5 then
+            JyMainAsync.Menu_PersonExit()
+        elseif r == 6 then
+            local result = JyMainAsync.Menu_System()
+            if result == 1 then
+                return  -- 退出所有菜单
+            end
+        end
     end
-    
-    MenuAsync.ShowMenuCoroutine(menu, 6, 0, CC.MainMenuX, CC.MainMenuY, 0, 0, 1, 1, CC.DefaultFont, C_ORANGE, C_WHITE)
 end
 
--- 系统子菜单（协程版本）
-function JyMainAsync.Menu_SystemCoroutine()
+-- 系统子菜单
+function JyMainAsync.Menu_System()
     local menu = {
-        {"读取进度", JyMainAsync.Menu_ReadRecordCoroutine, 1},
-        {"保存进度", JyMainAsync.Menu_SaveRecordCoroutine, 1},
-        {"离开游戏", JyMainAsync.Menu_ExitCoroutine, 1},
+        {"读取进度", nil, 1},
+        {"保存进度", nil, 1},
+        {"离开游戏", nil, 1},
     }
     
     local r = MenuAsync.ShowMenuCoroutine(menu, 3, 0, CC.MainSubMenuX, CC.MainSubMenuY, 0, 0, 1, 1, CC.DefaultFont, C_ORANGE, C_WHITE)
@@ -47,20 +68,29 @@ function JyMainAsync.Menu_SystemCoroutine()
         return 0
     elseif r < 0 then
         return 1
+    elseif r == 1 then
+        JyMainAsync.Menu_ReadRecord()
+        return 1
+    elseif r == 2 then
+        JyMainAsync.Menu_SaveRecord()
+        return 0
+    elseif r == 3 then
+        JyMainAsync.Menu_Exit()
+        return 1
     end
+    return 0
 end
 
--- 离开菜单（协程版本）
-function JyMainAsync.Menu_ExitCoroutine()
+-- 离开菜单
+function JyMainAsync.Menu_Exit()
     Cls()
     if AsyncMessageBox.ShowYesNoCoroutine(-1, -1, "是否真的要离开游戏？", C_WHITE, CC.DefaultFont) == true then
         JY.Status = GAME_END
     end
-    return 1
 end
 
--- 保存进度（协程版本）
-function JyMainAsync.Menu_SaveRecordCoroutine()
+-- 保存进度
+function JyMainAsync.Menu_SaveRecord()
     local menu = {
         {"进度一", nil, 1},
         {"进度二", nil, 1},
@@ -75,11 +105,10 @@ function JyMainAsync.Menu_SaveRecordCoroutine()
         SaveRecord(r)
         Cls(CC.MainSubMenuX2, CC.MainSubMenuY, CC.ScreenW, CC.ScreenH)
     end
-    return 0
 end
 
--- 读取进度（协程版本）
-function JyMainAsync.Menu_ReadRecordCoroutine()
+-- 读取进度
+function JyMainAsync.Menu_ReadRecord()
     local menu = {
         {"进度一", nil, 1},
         {"进度二", nil, 1},
@@ -88,47 +117,43 @@ function JyMainAsync.Menu_ReadRecordCoroutine()
     
     local r = MenuAsync.ShowMenuCoroutine(menu, 3, 0, CC.MainSubMenuX2, CC.MainSubMenuY, 0, 0, 1, 1, CC.DefaultFont, C_ORANGE, C_WHITE)
     
-    if r == 0 then
-        return 0
-    elseif r > 0 then
+    if r > 0 then
         DrawStrBox(CC.MainSubMenuX2, CC.MainSubMenuY, "请稍候......", C_WHITE, CC.DefaultFont)
         ShowScreen()
         LoadRecord(r)
         JY.Status = GAME_FIRSTMMAP
-        return 1
     end
 end
 
--- 状态子菜单（协程版本）
-function JyMainAsync.Menu_StatusCoroutine()
+-- 状态子菜单
+function JyMainAsync.Menu_Status()
     DrawStrBox(CC.MainSubMenuX, CC.MainSubMenuY, "要查阅谁的状态", C_WHITE, CC.DefaultFont)
     local nexty = CC.MainSubMenuY + CC.SingleLineHeight
-    
+
     local r = SelectTeamMenu(CC.MainSubMenuX, nexty)
     if r > 0 then
         ShowPersonStatus(r)
-        return 1
     else
         Cls()
-        return 0
     end
 end
 
--- 离队（协程版本）
-function JyMainAsync.Menu_PersonExitCoroutine()
+-- 离队菜单
+function JyMainAsync.Menu_PersonExit()
     DrawStrBox(CC.MainSubMenuX, CC.MainSubMenuY, "要求谁离队", C_WHITE, CC.DefaultFont)
     local nexty = CC.MainSubMenuY + CC.SingleLineHeight
-    
+
     local r = SelectTeamMenu(CC.MainSubMenuX, nexty)
-    
     if r == 1 then
         AsyncMessageBox.ShowMessageCoroutine(-1, -1, "抱歉！没有你游戏进行不下去", C_WHITE, CC.DefaultFont)
     elseif r > 1 then
         local personid = JY.Base["队伍" .. r]
         for i, v in ipairs(CC.PersonExit) do
             if personid == v[1] then
-                AsyncMessageBox.ShowMessageCoroutine(-1, -1, "抱歉！" .. JY.Person[personid]["姓名"] .. "不能离队", C_WHITE, CC.DefaultFont)
-                return 0
+                local AsyncGlobals = require("async_globals")
+                AsyncGlobals.install()
+                oldCallEvent(v[2])
+                AsyncGlobals.uninstall()
             end
         end
         
@@ -136,27 +161,28 @@ function JyMainAsync.Menu_PersonExitCoroutine()
             instruct_21(personid)
         end
     end
-    return 0
+    Cls()
 end
 
--- 医疗子菜单（协程版本）
-function JyMainAsync.Menu_DoctorCoroutine()
+-- 医疗子菜单
+function JyMainAsync.Menu_Doctor()
     DrawStrBox(CC.MainSubMenuX, CC.MainSubMenuY, "要医疗谁", C_WHITE, CC.DefaultFont)
     local nexty = CC.MainSubMenuY + CC.SingleLineHeight
     
     local r = SelectTeamMenu(CC.MainSubMenuX, nexty)
     if r > 0 then
         local id = JY.Base["队伍" .. r]
-        local id2
         
         if JY.Person[id]["体力"] < 10 then
             AsyncMessageBox.ShowMessageCoroutine(-1, -1, "体力不足，不能医疗", C_WHITE, CC.DefaultFont)
-            return 0
+            Cls()
+            return
         end
         
         if JY.Person[id]["受伤程度"] <= 0 and JY.Person[id]["中毒程度"] <= 0 then
             AsyncMessageBox.ShowMessageCoroutine(-1, -1, JY.Person[id]["姓名"] .. "生命正常，无需医疗", C_WHITE, CC.DefaultFont)
-            return 0
+            Cls()
+            return
         end
         
         local num = math.modf(JY.Person[id]["医疗能力"] * JY.Person[id]["生命上限"] / 100)
@@ -168,19 +194,15 @@ function JyMainAsync.Menu_DoctorCoroutine()
         JY.Person[id]["中毒程度"] = JY.Person[id]["中毒程度"] - num
         if JY.Person[id]["中毒程度"] < 0 then JY.Person[id]["中毒程度"] = 0 end
         
-        if JY.Person[id]["中毒程度"] > 0 then
-            AsyncMessageBox.ShowMessageCoroutine(-1, -1, string.format("%s 中毒程度减少 %d", JY.Person[id]["姓名"], num), C_ORANGE, CC.DefaultFont)
-        else
-            AsyncMessageBox.ShowMessageCoroutine(-1, -1, string.format("%s 生命增加 %d", JY.Person[id]["姓名"], num), C_ORANGE, CC.DefaultFont)
-        end
+        AsyncMessageBox.ShowMessageCoroutine(-1, -1, string.format("%s 生命增加 %d", JY.Person[id]["姓名"], num), C_ORANGE, CC.DefaultFont)
         
         JY.Person[id]["体力"] = JY.Person[id]["体力"] - 10
     end
-    return 0
+    Cls()
 end
 
--- 解毒子菜单（协程版本）
-function JyMainAsync.Menu_DecPoisonCoroutine()
+-- 解毒子菜单
+function JyMainAsync.Menu_DecPoison()
     DrawStrBox(CC.MainSubMenuX, CC.MainSubMenuY, "要为谁解毒", C_WHITE, CC.DefaultFont)
     local nexty = CC.MainSubMenuY + CC.SingleLineHeight
     
@@ -190,7 +212,8 @@ function JyMainAsync.Menu_DecPoisonCoroutine()
         
         if JY.Person[id]["中毒程度"] <= 0 then
             AsyncMessageBox.ShowMessageCoroutine(-1, -1, JY.Person[id]["姓名"] .. "没有中毒", C_WHITE, CC.DefaultFont)
-            return 0
+            Cls()
+            return
         end
         
         local num = math.modf(JY.Person[id]["解毒能力"] * JY.Person[id]["生命上限"] / 100)
@@ -201,11 +224,11 @@ function JyMainAsync.Menu_DecPoisonCoroutine()
         
         AsyncMessageBox.ShowMessageCoroutine(-1, -1, string.format("%s 中毒程度减少 %d", JY.Person[id]["姓名"], num), C_ORANGE, CC.DefaultFont)
     end
-    return 0
+    Cls()
 end
 
--- 物品子菜单（协程版本）
-function JyMainAsync.Menu_ThingCoroutine()
+-- 物品子菜单
+function JyMainAsync.Menu_Thing()
     local things = {}
     local num = 0
     
@@ -218,7 +241,8 @@ function JyMainAsync.Menu_ThingCoroutine()
     
     if num == 0 then
         AsyncMessageBox.ShowMessageCoroutine(-1, -1, "没有物品", C_WHITE, CC.DefaultFont)
-        return 0
+        Cls()
+        return
     end
     
     local x1 = (CC.ScreenW - 10 * CC.DefaultFont - 2 * CC.MenuBorderPixel) / 2
@@ -231,27 +255,7 @@ function JyMainAsync.Menu_ThingCoroutine()
         UseThingSelect(thingid)
     end
     
-    return 0
-end
-
--- 物品使用选择（协程版本）
-function JyMainAsync.UseThingSelectCoroutine(thingid)
-    local thingType = JY.Thing[thingid]["类型"]
-    
-    if thingType == 0 then
-        -- 药品，选择使用对象
-        DrawStrBox(CC.MainSubMenuX, CC.MainSubMenuY, "要给谁使用", C_WHITE, CC.DefaultFont)
-        local nexty = CC.MainSubMenuY + CC.SingleLineHeight
-        local r = SelectTeamMenu(CC.MainSubMenuX, nexty)
-        
-        if r > 0 then
-            local id = JY.Base["队伍" .. r]
-            UseThing(id, thingid)
-        end
-    else
-        -- 其他物品
-        UseThing(0, thingid)
-    end
+    Cls()
 end
 
 return JyMainAsync
